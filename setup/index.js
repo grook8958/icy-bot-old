6f63bfb2c9fcd5053aad0fba608fc82c32e85afd
grook8958 <clement.pusks@gmail.com>
2021-07-28 20:22:48 +0200

    Updated setup

--- setup/index.js
+++ setup/index.js
@@ -2,7 +2,7 @@ const dir = require('./dir.json');
 
 const fs = require('fs');
 
-const data = 'TOKEN=\nDEFAULT_PREFIX=!\nMONGO_URI='
+const data = 'TOKEN=\nDEFAULT_PREFIX=!\nMONGO_URI=\nREDIS_URI='
 
 fs.writeFile(`${dir.directory}/.env`, data, 'utf8', function(err) {
     console.log('Successfully created .env file');
