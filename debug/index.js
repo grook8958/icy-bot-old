fbd830aa13d91c0b4fe144fd23eb555689f91c52
grook8958 <clement.puks@gmail.com>
2021-08-18 16:06:09 +0200

    Added some comments

--- debug/index.js
+++ debug/index.js
@@ -1,20 +1,25 @@
+//Import dependencies
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
 
+//Create wait function
 const wait = require('util').promisify(setTimeout);
 
+//Desctructure some methods from fs
 const { readFile, writeFile } = fs;
 
+//Define problems
 const problems = {};
 
-const red = chalk.red;
+//Define logging color function
 const error = chalk.red.bgBlack;
 const yellow = chalk.bold.yellow;
 
+//Add missingDotEnvField property to problems
 problems.missingDotEnvField = [];
 
-
+//Debug .env
 const debugDotEnv = () => {
   //Read content of .env file
   const file = '.env';
@@ -41,47 +46,60 @@ const debugDotEnv = () => {
     }
   });
 }
+//Inform the user that the debug started
 console.log(yellow('[DEBUGGER] Starting Debug...'));
 
 
 
 const debug = async () => {
+  //Get full path to the logs directory
   const logDir = path.resolve('./debug/logs')
 
   //Debug dotEnv file
   debugDotEnv();
 
+  //Wait a little for the debugging to finish
   await wait('750');
 
+  //Inform the user that the debug ended
   console.log(yellow('[DEBUGGER] END OF DEBUG'));
   
+  //Get & format the data
   const currentDate = new Date();
   const cDay = currentDate.getDate();
   const cMonth = currentDate.getMonth() + 1;
   const cYear = currentDate.getFullYear();
-
   const date = `${cDay}-${cMonth}-${cYear}`
+
+  //Full file path & name
   const logFilePath = `${logDir}/log_${date}.txt`
   
+  //Content of log file
   let data = `**DEBUG LOG** - ${date}\n.env Issues:`
 
+  //Check if their are any .env regarded problems
   if (!problems.missingDotEnvField.length === 0) {
+    //Loop through all the problems
     for (const missingDotEnvField of problems.missingDotEnvField) {
+      //Add to the log file each problems
       data += `\n- ${missingDotEnvField}`;
     }
   } else {
+    //Add to the log file that there is no issues
     data += 'No issues';
   }
 
+  //Tell the user where he can find the log
   console.log(`A complete log can be found ${logFilePath}`);
 
-
+  //Write the log file
   writeFile(logFilePath, data, err => {
     if (err) throw err;
   })
 
 }
 
+//Run debug
 debug();
 
 
